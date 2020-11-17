d6d9c87df061a4241abee6fd1933e577d1504d29
Jared Angel <jared.aescobedo@gmail.com>
2020-11-17 13:54:30 -0600

    initial commit

---
+++ src/App/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+class App extends React.Component {
+  state = {
+    voyages: [],
+    activities: []
+  };
+
+  componentDidMount() {
+    // fetches
+  }
+
+  
+
+  render() {
+    return;
+  }
+  
+}
+
+export default App;
